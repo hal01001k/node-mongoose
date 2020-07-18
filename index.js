bda7ee2399ef38bd4abc21dc72014534869f6927
hal01001k <hiteshdaksh@yahoo.co.uk>
2020-07-18 20:01:33 +0530

    mongoose with commets schema

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const Dishes  =  require('./models/dishes');
+const Dishes = require('./models/dishes');
 
 const url = 'mongodb://localhost:27017/confusion';
 const connect = mongoose.connect(url);
@@ -7,22 +7,34 @@ connect.then((db) => {
 
     console.log('Connected correctly to server');
 
-    var newDish = Dishes({
+    Dishes.create({
         name: 'Uthappizza',
         description: 'test'
-    });
+    }).then((dish) => {
+        console.log(dish);
 
-    newDish.save()
+        return Dishes.findByIdAndUpdate(dish._id, {
+            $set: { description: 'Updated test'}
+        },{
+            new: true
+        })
+            .exec();
+    })
         .then((dish) => {
             console.log(dish);
 
-            return Dishes.find({});
-        })
-        .then((dishes) => {
-            console.log(dishes);
+            dish.comments.push({
+                rating: 5,
+                comment: 'I\'m getting a sinking feeling!',
+                author: 'Leonardo di Carpaccio'
+            });
 
-            return Dishes.remove({});
-        })
+            return dish.save();
+        }).then((dish) => {
+        console.log(dish);
+
+        return Dishes.remove({});
+    })
         .then(() => {
             return mongoose.connection.close();
         })
